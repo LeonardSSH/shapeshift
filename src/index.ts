b6920e2a06dc09d4b3ac04e681ae356c3d03ca2e
Antonio Román <kyradiscord@gmail.com>
2021-12-12 23:09:11 +0100

    chore: fail

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,5 @@ export const s = new Shapes();
 export * from './lib/Result';
 export * from './lib/errors/ConstraintError';
 export * from './lib/errors/ValidationError';
+export * from './lib/errors/UnknownPropertyError';
 export * from './lib/errors/ExpectedValidationError';
